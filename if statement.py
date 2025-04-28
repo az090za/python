896e9b35d86effb8e5fc2d8c3d7375a9db687aaf
az090za <azza.alharthia@gmail.com>
2025-04-28 07:00:46 +0400

    Create if statement.py

---
+++ if statement.py
@@ -0,0 +1,11 @@
+N1=int(input("Enter N1 " ))
+N2=int(input("Enter N2 " ))
+
+print(N1,N2)
+
+if N1 > N2:
+    print("N1 is grater then N2")
+else:
+    print("N2 is grater then N1")
+      
+    print("Code Ended")
