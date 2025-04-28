84944de92d411622b5b50b0921de32f57ca39b25
az090za <azza.alharthia@gmail.com>
2025-04-28 06:57:36 +0400

    Create ID and Grades using loop to find the grades.py

---
+++ ID and Grades using loop to find the grades.py
@@ -0,0 +1,11 @@
+ID=[1,2,3,4,5,6,7,8,9,10]
+G=[90,80,80,67,79,56,90,95,88,100]
+print(ID)
+print(G)
+
+for i in range(len(G)):
+    if G[i]>80:
+        print(ID[i], end=" ")
+
+print()
+print("Azza")        
