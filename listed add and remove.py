65968673fc6754709f271c28aa2ab82d4d2ff383
az090za <azza.alharthia@gmail.com>
2025-04-28 07:12:36 +0400

    Create listed add and remove.py

---
+++ listed add and remove.py
@@ -0,0 +1,16 @@
+devices = ["Router", "Switch", "Firewall", "Access Point","Server"]
+print(devices)
+
+#devices.append("Server")
+#print(devices)
+
+devices.insert(1,"Server")
+print(devices)
+print(devices.count("Server"))
+
+#devices.remove("Server")
+#print(devices)
+
+A=[1,2,4,1,1,5]
+#print(A.count(1))
+print(len(A))
