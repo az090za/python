880d797c6e44aa2105058eabf950c920f4b79ddf
az090za <azza.alharthia@gmail.com>
2025-04-28 06:36:13 +0400

    Create add time in split.py

---
+++ add time in split.py
@@ -0,0 +1,10 @@
+T1=input("Enter time1 ")
+T2=input("Enter time2 ")
+
+H1=int(T1.split(":")[0])
+M1=int(T1.split(":")[1])
+H2=int(T2.split(":")[0])
+M2=int(T2.split(":")[1])
+
+print(H1,M1,H2,M2)
+    
