220f3d0c5f79f487326202f6b5557ec9abe34155
az090za <azza.alharthia@gmail.com>
2025-04-28 06:59:45 +0400

    Create if else using network models.py

---
+++ if else using network models.py
@@ -0,0 +1,19 @@
+num = int(input("Enter the number: "))
+
+if num == 1:
+   print("Physical")  
+elif num == 2:
+     print("Data Link")
+elif num == 3:
+     print("Network")
+elif num == 4:
+     print("Transport")     
+elif num == 5:
+     print("Session")
+elif num == 6:
+     print("Presentation")
+elif num == 7:
+     print("Application")     
+     
+else:
+    print(" ")
