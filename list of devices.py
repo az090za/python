ee39c939fb66e21e2daa9694c9173ad652dc137c
az090za <azza.alharthia@gmail.com>
2025-04-28 07:07:59 +0400

    Create list of devices.py

---
+++ list of devices.py
@@ -0,0 +1,8 @@
+devices = ["Router", "Switch", "Firewall", "Access Point"]
+print(devices)
+print(devices[2])
+
+port_status=["Used", "Free", "Free", "Used"]
+print(port_status)
+port_status[0]="Free"
+print(port_status)
